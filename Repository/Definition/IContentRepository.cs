21944dbc5c9e61ae62e0ec62595bf4f69ea47f96
Chris McBride <christopher.mcbride@gmail.com>
2018-10-25 18:38:55 -0400

    demo code for tree publish

---
+++ Repository/Definition/IContentRepository.cs
@@ -0,0 +1,17 @@
+﻿using KT.Content.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Contentful.Core.Models.Management;
+using Contentful.Core.Models;
+
+namespace KT.Content.Data.Definition
+{
+    public interface IContentRepository
+    {
+        Task<int> PublishItem(string slug);
+        Task<Project> GetProject(string slug, bool preview);
+        Task<Snapshot> GetLastSnapshot(string id);
+        Task<Entry<dynamic>> GetItemFromMgmtAPI(string id);
+        Task<Entry<dynamic>> UpdateEntry(dynamic entry);
+    }
+}
